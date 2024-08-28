e373370de5c12b6328984902253f800427bded1b
Mihajlo Varga <93919324+meksoni@users.noreply.github.com>
2024-08-28 19:35:25 +0200

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,21 +1,10 @@
 <?php
 
-// $db_name = 'mysql:host=mysql.cnt.rs;dbname=10011_xydesign';
-// $user_name = '10011_xydesign';
-// $user_password = 'rfEpNnJ2a152P6N5';
-// $conn = new PDO($db_name, $user_name, $user_password);
-
-?>
-
-
-
-<?php
-
 // FOR LOCAL HOST
 
 
 $db_name = 'mysql:host=localhost;dbname=shop_db';
- $user_name = 'root';
+$user_name = 'root';
 $user_password = '';
  $conn = new PDO($db_name, $user_name, $user_password);
 ?>
